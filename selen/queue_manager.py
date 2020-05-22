e978d3c1b5baa44a55031d92105c2c63d2473b77
medram <mramouchy@gmail.com>
2020-05-22 01:28:55 +0000

    main queue

--- selen/queue_manager.py
+++ selen/queue_manager.py
@@ -1,15 +1,37 @@
 import queue
+import threading
 
+print(threading.enumerate())
+exit()
 
 class MainQueue(queue.Queue):
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
 		# a list of taskAdapter queues.
 		self.queue_list = []
+		self._iter = iter(self.queue_list)
+		self._lock = threading.Lock()
+
 
 	def get(self):
-		pass
-	
+		""" Simulate get queue function """
+		
+		while True:
+			# deny threads from getting access
+			self._lock.acquire()
+			try:
+				q = next(self._iter)
+				return q.get(False)
+			except StopIteration:
+				self._iter = iter(self.queue_list)
+				time.sleep(0.5)
+			except queue.Empty:
+				# raise StopIteration()
+				time.sleep(0.5)
+			# allow threads to get access
+			self._lock.release()
+
+
 	def register(self, q):
 		""" register a taskAdapter queue """
 		self.queue_list.append(q)
@@ -17,6 +39,12 @@ class MainQueue(queue.Queue):
 	def unregister(self, q):
 		""" unregister a taskAdapter queue """
 		del self.queue_list[q]
+
+	def __next__(self):
+		return self.get()
+
+	def __iter__(self):
+		return self
 
 
 class QueueManager:
@@ -32,6 +60,14 @@ class QueueManager:
 		if not cls._instance:
 			cls._instance = cls()
 		return cls._instance
+
+	def register(self, q):
+		""" register a taskAdapter queue """
+		self.main_queue.queue_list.append(q)
+
+	def unregister(self, q):
+		""" unregister a taskAdapter queue """
+		del self.main_queue.queue_list[q]
 
 	def get_main_queue():
 		""" Return the main queue """
