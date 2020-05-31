78acbc701eb29321f769947c2fd951356467279c
medram <mramouchy@gmail.com>
2020-05-31 22:58:07 +0100

    Some leanup

--- universalbot/tasks.py
+++ universalbot/tasks.py
@@ -30,29 +30,6 @@ def run_lists(task_id):
 		ApprovedTaskManager.register(task_id)
 		print(f'-> The Task ({task_id}) is pushed to Pools.')
 	
-	# try:
-	# 	task = TaskAdaptor.objects.get(pk=task_id)
-	# except TaskAdaptor.DoesNotExist:
-	# 	pass
-	# else:
-	# 	task.current = 0
-	# 	task.progress = 0
-	# 	task.save()
-
-	# 	task_started.send(TaskAdaptor, task=task)
-
-	# 	lists = task.lists.all()
-
-	# 	for l in lists:
-	# 		for profile in l.profiles.all():
-	# 			def sub_task():
-	# 				return run_profile(profile, l, task)
-	# 			# add a sub task to the main queue.
-	# 			task._queue.put(sub_task)
-
-	# 			# register a queue to QueueManager
-	# 			QM.register(task._queue)
-
 	print('Done!')
 
 	task_finished.send(TaskAdaptor, task=task)
