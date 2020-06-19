5167890ce30f96ffdab9c6a41c274c35a248fcb1
medram <mramouchy@gmail.com>
2020-06-19 10:43:14 +0100

    migrations/manage.py fix

--- app/settings.py
+++ app/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'background_task',
-    'universalbot.apps.UniversalbotConfig',
+    'universalbot',
     'extra_settings',
     'settings',
     'multiselectfield',
