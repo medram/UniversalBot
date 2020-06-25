4b034b30c8dac99afd19d9e461024daeb64b3f05
medram <mramouchy@gmail.com>
2020-06-25 23:28:24 +0100

    added my github account URL

--- app/settings.py
+++ app/settings.py
@@ -149,5 +149,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 APP_NAME = 'UniversalBot'
 APP_VERSION = '1.0.0'
 POWRED_BY = 'Mohammed Ramouchy'
-POWRED_BY_URL = 'https://ramouchy.com'
+POWRED_BY_URL = 'https://github.com/medram'
 POWRED_BY_EMAIL = 'mohammed@ramouchy.com'
