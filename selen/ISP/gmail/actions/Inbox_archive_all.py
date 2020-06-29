323d2693aec3bc011e9c7a79ebee08d7b2a4da53
medram <mramouchy@gmail.com>
2020-06-29 13:36:27 +0100

    Fixed: importing a selenium exception

--- selen/ISP/gmail/actions/Inbox_archive_all.py
+++ selen/ISP/gmail/actions/Inbox_archive_all.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
 
 from selen.abstract import ActionAbstract
 from selen import utils, exceptions
